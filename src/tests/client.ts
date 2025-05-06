b2a9242d8e00602cbc7ba6e84b734a1c79355955
plasma119 <leechikwok@gmail.com>
2025-05-06 12:57:25 +0900

    removing deprecated tests

--- src/tests/client.ts
+++
@@ -1,7 +0,0 @@
-import HiveCommand from "../os/lib/hiveCommand.js";
-import HiveOS from "../os/os.js";
-
-export function main(os: HiveOS) {
-    let p = new HiveCommand('clientProgram');
-    os.registerShellProgram(p);
-}
--- src/tests/server.ts
+++
@@ -1,5 +0,0 @@
-import HiveOS from "../os/os.js";
-
-export function main(os: HiveOS) {
-    os.kernel.program.stdIO.input('net listen');
-}
