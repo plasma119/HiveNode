8053a8eb110d755270a405844766cf54c44ed7bd
plasma119 <leechikwok@gmail.com>
2025-05-06 14:20:48 +0900

    added more methods to CircularArray

--- src/lib/circularArray.ts
+++ src/lib/circularArray.ts
@@ -1,16 +1,37 @@
+// TODO: proper testing
+
 export class CircularArray<T> {
     maxSize: number;
 
     _array: T[] = [];
     _pointer: number = 0;
+    _size: number = 0;
 
     constructor(maxSize: number) {
-        this.maxSize = maxSize
+        this.maxSize = maxSize;
     }
 
     push(item: T) {
         this._array[this._pointer++] = item;
         if (this._pointer >= this.maxSize) this._pointer = 0;
+        this._size++;
+        if (this._size > this.maxSize) this._size = this.maxSize;
+    }
+
+    pop() {
+        if (this.maxSize == 0) return undefined;
+        this._pointer--;
+        if (this._pointer < 0) this._pointer = this.maxSize - 1;
+        const item = this._array[this._pointer];
+        delete this._array[this._pointer];
+        return item;
+    }
+
+    get(index: number) {
+        if (index < 0 || index >= this.maxSize) return undefined;
+        let j = this._pointer - this._size + index;
+        if (j < 0) j += this.maxSize;
+        return this._array[j];
     }
 
     slice(start?: number, end?: number) {
@@ -27,10 +48,11 @@ export class CircularArray<T> {
     clear() {
         this._array = [];
         this._pointer = 0;
+        this._size = 0;
     }
 
     size() {
-        return this._array.length;
+        return this._size;
     }
 
     resize(size: number) {
@@ -39,6 +61,7 @@ export class CircularArray<T> {
         if (size < currentArray.length) {
             this._array = currentArray.slice(currentArray.length - size);
             this._pointer = 0;
+            this._size = size;
         } else {
             this._array = currentArray;
             this._pointer = currentArray.length;
